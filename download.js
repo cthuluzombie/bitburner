3d85b8a89845d7a79d3bbe1876d841b0f3a1cb89
cthuluzombie <86600477+cthuluzombie@users.noreply.github.com>
2023-05-31 12:00:01 +1000

    Create download.js

---
+++ download.js
@@ -0,0 +1,33 @@
+export async function main(ns) {
+    const { rootUrl } = ns.flags([['rootUrl', 'raw.githubusercontent.com/cthuluzombie/bitburner/main/']]); 
+    const folder = 'scripts';
+
+    const files = [
+            "dev.js",
+            "hacknet-bot.js",
+            "money.script",
+            "scan.js",
+            "worm/later.script",
+            "worm/sooner.script",
+            "ect/purchase-server-8gb.script",
+            "ect/sigma-cosmetics.script"
+    ];
+    
+    let filesImported = true;
+    for (let file of files) {
+        const remoteFileName = `${rootUrl}scripts/${file}?t=${Date.now()}`;
+        const result = await ns.wget(remoteFileName, `/${folder}/${file}`);
+        filesImported = filesImported && result;
+        ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
+    }
+
+    ns.tprint('='.repeat(20));
+    
+    if (filesImported) {
+        ns.tprint('Hey! Thank you for downloading the BitBurner Scripts.');
+        ns.tprint(`You've installed these in the ${folder} directory.`);
+        ns.tprint(`A good place to start is running \`run /${folder}/hax.js\``);
+    } else {
+        ns.tprint('You had some issues downloading files, please reach out to the repo maintainer or check your config.');
+    }
+}
