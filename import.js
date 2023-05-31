c5473e60101c7eef0fc7731eb999557456e601de
cthuluzombie <86600477+cthuluzombie@users.noreply.github.com>
2023-05-31 12:03:10 +1000

    Update import.js

--- import.js
+++ import.js
@@ -1,5 +1,5 @@
 export async function main(ns) {
-  const rootUrl = 'raw.githubusercontent.com/cthuluzombie/bitburner/main/';
+  const rootUrl = 'https://raw.githubusercontent.com/cthuluzombie/bitburner/main/';
   const downloadFileName = `${rootUrl}download.js?t=${Date.now()}`;
   await ns.wget(downloadFileName, `/scripts/download.js`);
   ns.run(`/scripts/download.js`, 1, `--rootUrl=${rootUrl}`);
